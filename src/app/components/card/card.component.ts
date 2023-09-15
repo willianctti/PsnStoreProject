2aa50cc3098361645ffa1c1085f73e95541c2685
willianctti <willianctti@gmail.com>
2023-09-15 18:28:50 -0300

    finalizando projeto

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit{
+
+  @Input()
+  gameCover: string = ""
+  @Input()
+  gameLabel: string = ""
+  @Input()
+  gameType: string = "Digital PS4";
+  @Input()
+  gamePrice: string = "R$399,90"
+
+  
+  
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
